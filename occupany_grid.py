7d00f5a4087fef0cb42937d473598ad6400da3f0
Nathan Faber <ntfaber@gmail.com>
2023-02-20 15:31:35 -0500

    vectorize setting and query line

--- occupany_grid.py
+++ occupany_grid.py
@@ -7,81 +7,81 @@ import sys
 from typing import Sequence
 import numpy as np
 
-Point2d = namedtuple("Point", "x y")
+# Point2d = namedtuple("Point", "x y")
 Point3d = namedtuple("Point3", "x y z")
 
 Pose2d = namedtuple("Pose2d", "x y h")
 
 
-class OccupanyGrid2d:
-    """Occupancy Grid written to use cm as units."""
+# class OccupanyGrid2d:
+#     """Occupancy Grid written to use cm as units."""
 
-    def __init__(
-        self, grid_width: int, grid_height: int, origin: Point2d, cell_size=5
-    ) -> None:
-        if (
-            grid_width % 2 == 1
-            or grid_height % 2 == 1
-            or grid_height <= 1
-            or grid_height <= 1
-        ):
-            raise ValueError("Occupancy grid should be even shaped")
-        if origin.x >= cell_size * grid_width or origin.y >= cell_size * grid_height:
-            raise ValueError("Specify an origin within the Occupancy Grid")
-        self.origin = origin  # in CM not in cells
-        self.grid_width = grid_width
-        self.grid_height = grid_height
+#     def __init__(
+#         self, grid_width: int, grid_height: int, origin: Point2d, cell_size=5
+#     ) -> None:
+#         if (
+#             grid_width % 2 == 1
+#             or grid_height % 2 == 1
+#             or grid_height <= 1
+#             or grid_height <= 1
+#         ):
+#             raise ValueError("Occupancy grid should be even shaped")
+#         if origin.x >= cell_size * grid_width or origin.y >= cell_size * grid_height:
+#             raise ValueError("Specify an origin within the Occupancy Grid")
+#         self.origin = origin  # in CM not in cells
+#         self.grid_width = grid_width
+#         self.grid_height = grid_height
 
-        self.cell_size = cell_size
+#         self.cell_size = cell_size
 
-        # map_width_cells = 1 + math.ceil((upper_right.x-cell_size)/self.cell_size) + math.ceil((bottom_left.x+cell_size)/self.cell_size)
-        # map_height_cells = 1 + math.ceil((upper_right.y-cell_size)/self.cell_size)
-        self.map = np.zeros((self.grid_width, grid_height), dtype=bool)
+#         # map_width_cells = 1 + math.ceil((upper_right.x-cell_size)/self.cell_size) + math.ceil((bottom_left.x+cell_size)/self.cell_size)
+#         # map_height_cells = 1 + math.ceil((upper_right.y-cell_size)/self.cell_size)
+#         self.map = np.zeros((self.grid_width, grid_height), dtype=bool)
 
-    @staticmethod
-    def glbl_pt_to_cell(
-        origin: Point2d, cell_size: float, point: Point2d
-    ) -> tuple[int, int]:
-        return (
-            math.floor((point.x + origin.x) / cell_size),
-            math.floor((point.y + origin.y) / cell_size),
-        )
+#     @staticmethod
+#     def glbl_pt_to_cell(
+#         origin: Point2d, cell_size: float, point: Point2d
+#     ) -> tuple[int, int]:
+#         return (
+#             math.floor((point.x + origin.x) / cell_size),
+#             math.floor((point.y + origin.y) / cell_size),
+#         )
 
-    def add_points_global(self, points: Sequence[namedtuple]):
-        """Add points in the global frame (relative to origin) to the occupancy_grid."""
-        # TODO: Raise error on too big of query
+#     def add_points_global(self, points: Sequence[namedtuple]):
+#         """Add points in the global frame (relative to origin) to the occupancy_grid."""
+#         # TODO: Raise error on too big of query
 
-        cells = [self.glbl_pt_to_cell(self.origin, self.cell_size, pt) for pt in points]
-        for i in cells:
-            self.map[i] = True
-        # TODO: (COuldnt get this wokring vectorized :(
+#         cells = [self.glbl_pt_to_cell(self.origin, self.cell_size, pt) for pt in points]
+#         for i in cells:
+#             self.map[i] = True
+#         # TODO: (COuldnt get this wokring vectorized :(
 
-    def get_points_global(self, points: Sequence[namedtuple]):
-        """Take Points in the global Mpa frame and return if they are occupied"""
-        return [
-            self.map[self.glbl_pt_to_cell(self.origin, self.cell_size, pt)]
-            for pt in points
-        ]
+#     def get_points_global(self, points: Sequence[namedtuple]):
+#         """Take Points in the global Mpa frame and return if they are occupied"""
+#         return [
+#             self.map[self.glbl_pt_to_cell(self.origin, self.cell_size, pt)]
+#             for pt in points
+#         ]
 
-    def get_points_rel(pose: Pose2d, points: Sequence[namedtuple]):
-        # TODO: Matrix multiplication for rotation and transformation and then query points
-        raise NotImplementedError
+#     def get_points_rel(pose: Pose2d, points: Sequence[namedtuple]):
+#         # TODO: Matrix multiplication for rotation and transformation and then query points
+#         raise NotImplementedError
 
-    def set_points_rel(pose: Pose2d, points: Sequence[namedtuple]):
-        # TODO: Matrix multiplication for rotation and transformation and then set points
-        raise NotImplementedError
+#     def set_points_rel(pose: Pose2d, points: Sequence[namedtuple]):
+#         # TODO: Matrix multiplication for rotation and transformation and then set points
+#         raise NotImplementedError
 
-    def add_rectangles(self, bottom_left_point: Point2d, top_right_point: Point2d):
-        """Helper to add rows in our environment. Positioned on our gloabl coordinate"""
-        bottom_left_cell = self.glbl_pt_to_cell(
-            self.origin, self.cell_size, bottom_left_point
-        )
-        top_right_cell = self.glbl_pt_to_cell(
-            self.origin, self.cell_size, top_right_point
-        )
-        for x in range(bottom_left_cell[0], top_right_cell[0] + 1):
-            for y in range(bottom_left_cell[1], top_right_cell[1] + 1):
-                self.map[(x, y)] = True
+#     def add_rectangles(self, bottom_left_point: Point2d, top_right_point: Point2d):
+#         """Helper to add rows in our environment. Positioned on our gloabl coordinate"""
+#         bottom_left_cell = self.glbl_pt_to_cell(
+#             self.origin, self.cell_size, bottom_left_point
+#         )
+#         top_right_cell = self.glbl_pt_to_cell(
+#             self.origin, self.cell_size, top_right_point
+#         )
+#         for x in range(bottom_left_cell[0], top_right_cell[0] + 1):
+#             for y in range(bottom_left_cell[1], top_right_cell[1] + 1):
+#                 self.map[(x, y)] = True
 
 
 class OccupanyGrid3d:
@@ -104,7 +104,11 @@ class OccupanyGrid3d:
             or grid_height <= 1
         ):
             raise ValueError("Occupancy grid should be even shaped")
-        if origin.x >= cell_size * grid_width or origin.y >= cell_size * grid_depth or origin.z >= cell_size * grid_height:
+        if (
+            origin.x >= cell_size * grid_width
+            or origin.y >= cell_size * grid_depth
+            or origin.z >= cell_size * grid_height
+        ):
             raise ValueError("Specify an origin within the Occupancy Grid")
         self.origin = origin  # in CM not in cells
 
@@ -127,18 +131,23 @@ class OccupanyGrid3d:
     def add_points_global(self, points: Sequence[namedtuple]):
         """Add points in the global frame (relative to origin) to the occupancy_grid."""
         # TODO: Raise error on too big of query
+        cells = OccupanyGrid3d.glbl_pt_to_cell(self.origin, self.cell_size, points)
+        transposed = cells.transpose()
+        self.map[(transposed[0], transposed[1], transposed[2])] = True
 
-        cells = [self.glbl_pt_to_cell(self.origin, self.cell_size, pt) for pt in points]
-        for i in cells:
-            self.map[i] = True
-        # TODO: (COuldnt get this wokring vectorized :(
-
-    def get_points_global(self, points: Sequence[namedtuple]):
+    def get_points_global(self, points: np.ndarray):
         """Take Points in the global Mpa frame and return if they are occupied"""
-        return [
-            self.map[self.glbl_pt_to_cell(self.origin, self.cell_size, pt)]
-            for pt in points
-        ]
+        cells = OccupanyGrid3d.glbl_pt_to_cell(self.origin, self.cell_size, points)
+        transposed = cells.transpose()
+        return self.map[(transposed[0], transposed[1], transposed[2])]
+
+    @staticmethod
+    def glbl_pt_to_cell(
+        origin: Point3d, cell_size: float, points: np.ndarray
+    ) -> np.ndarray:
+        transformed = points + np.array(origin)
+        scaled = np.floor(transformed / cell_size).astype("int")
+        return scaled
 
     def get_points_rel(pose: Pose2d, points: Sequence[namedtuple]):
         # TODO: Matrix multiplication for rotation and transformation and then query points
@@ -148,24 +157,61 @@ class OccupanyGrid3d:
         # TODO: Matrix multiplication for rotation and transformation and then set points
         raise NotImplementedError
 
-    def add_rectangles(self, bottom_left_point: Point2d, top_right_point: Point2d):
+    def add_rectangles(self, bottom_left_point: Point3d, top_right_point: Point3d):
         """Helper to add rows in our environment. Positioned on our gloabl coordinate"""
-        bottom_left_cell = self.glbl_pt_to_cell(
+        # TODO: Refactor to use linspace to then use numpy indexing
+        # self.map[(range(bottom_left_cell[0], top_right_cell[0] + 1), range(bottom_left_cell[1], top_right_cell[1] + 1), range(bottom_left_cell[2], top_right_cell[2] + 1))]
+
+        bottom_left_cell = OccupanyGrid3d.glbl_pt_to_cell(
             self.origin, self.cell_size, bottom_left_point
         )
-        top_right_cell = self.glbl_pt_to_cell(
+        top_right_cell = OccupanyGrid3d.glbl_pt_to_cell(
             self.origin, self.cell_size, top_right_point
         )
-        for x in range(bottom_left_cell[0], top_right_cell[0] + 1):
-            for y in range(bottom_left_cell[1], top_right_cell[1] + 1):
-                for z in range(bottom_left_cell[2], top_right_cell[2] + 1):
-                    self.map[(x, y, z)] = True
+        xs = range(bottom_left_cell[0], top_right_cell[0] + 1)
+        ys = range(bottom_left_cell[1], top_right_cell[1] + 1)
+        zs = range(bottom_left_cell[2], top_right_cell[2] + 1)
+
+        for x in xs:
+            for y in ys:
+                for z in zs:
+                    self.map[(x,y,z)] = True
+
+    def check_line(
+        self, start_pt: Point3d, end_pt: Point3d, bounding_box: list[Point3d]
+    ):
+        """Take in points in the global frame (ie. 0,0 is the origin)
+
+        Args:
+            start_pt (Point3d): _description_
+            end_pt (Point3d): _description_
+
+        Returns: True if there is no obstacle map collision
+        """
+        start_vec = np.array(start_pt)
+        end_vec = np.array(end_pt)
+        length = np.linalg.norm(end_vec - start_vec)
+        num_pts = int(length / (self.cell_size / 4))
+
+        points = np.linspace(start_vec, end_vec, num=num_pts, endpoint=True)
+        points_list = np.concatenate([points + np.array(c) for c in bounding_box])
+
+        return not np.any(self.get_points_global(points_list))
 
 
 if __name__ == "__main__":
-    omap = OccupanyGrid2d(4, 6, Point2d(15, 10), 10)
     omap3d = OccupanyGrid3d(4, 6, 2, Point3d(15, 10, 0), 10)
-    omap3d.add_rectangles(Point3d(0,0,0), Point3d(10,10, 19))
+    omap3d.add_rectangles(Point3d(0, 0, 0), Point3d(10, 10, 19))
+
+    quad_corners = [
+        Point3d(0, 0, 0),
+        Point3d(5, 5, 0),
+        Point3d(-5, 5, 0),
+        Point3d(5, -5, 0),
+        Point3d(-5, -5, 0),
+    ]
+    valid_line = omap3d.check_line(Point3d(0, 0, 0), Point3d(11, 21, 0), quad_corners)
+    print(valid_line)
     # Some gut checks here:
     # print(omap.map)
     # omap.add_points_global([Point2d(10,10), Point2d(0,0) ])
--- viz.py
+++ viz.py
@@ -4,78 +4,111 @@ from tkinter import Text
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import plotly.graph_objects as go
 
-from occupany_grid import OccupanyGrid2d, OccupanyGrid3d, Point3d
- 
-def plot_omap3d(Omap: OccupanyGrid2d):
-    axes = omap.size
+from occupany_grid import OccupanyGrid3d, Point3d
 
+
+class viz_world():
+    def __init__(self, omap: OccupanyGrid3d):
+        self.omap = omap
 
 
 if __name__ == "__main__":
     # The Robosys Environment
-    origin = Point3d(200,100, 0) # Bottom Left corner of grid is 2 meters to the left (x) and 1m negative y
-    grid_width = 100 # 5 meters wide (x)
-    grid_depth = 60 # 3 meters tall (y)
-    grid_height = 40 # 2 meters tall (z)
-    robosys_grid = OccupanyGrid3d(grid_width, grid_depth, grid_height, origin, cell_size=5)
-    robosys_grid.add_rectangles(Point3d(0,20,0), Point3d(200, 40, 60)) # Measurements in CM relative to origin
+    origin = Point3d(
+        200, 100, 0
+    )  # Bottom Left corner of grid is 2 meters to the left (x) and 1m negative y
+    grid_width = 100  # 5 meters wide (x)
+    grid_depth = 60  # 3 meters tall (y)
+    grid_height = 40  # 2 meters tall (z)
+    robosys_grid = OccupanyGrid3d(
+        grid_width, grid_depth, grid_height, origin, cell_size=5
+    )
+    robosys_grid.add_rectangles(
+        Point3d(0, 20, 0), Point3d(200, 40, 60)
+    )  # Measurements in CM relative to origin
     robosys_grid.add_rectangles(Point3d(-30,-60,0), Point3d(170, -20, 40))
 
     axes_shape = list(robosys_grid.map.shape)
-    
+
     # Create Data
     data = robosys_grid.map
-    
-    # Control Transparency
-    alpha = 0.2
-    
-    # Control colour
-    colors = np.empty(axes_shape + [4], dtype=np.float32)
-    
-    colors[:] = [1, 0, 0, alpha]  # red
-    
-    # Plot figure
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_box_aspect(axes_shape)  # aspect ratio is 1:1:1 in data space
-    ax.set_xlabel('X', fontsize=20)
-    ax.set_ylabel('Y')
-    
-    # Voxels is used to customizations of the
-    # sizes, positions and colors.
-    origin_cell = robosys_grid.glbl_pt_to_cell(Point3d(0,0,0), robosys_grid.cell_size, Point3d(robosys_grid.origin.x, robosys_grid.origin.y, robosys_grid.origin.z))
 
-    ax.scatter(origin_cell[0], origin_cell[1], origin_cell[2], s=8, marker='^')
+    # Convert Plot coorinates to world coordinates
+    plots = np.array(data.nonzero()).transpose()
+    adj = (plots*robosys_grid.cell_size - robosys_grid.origin).transpose()
 
-    filled = np.transpose(data.nonzero())
-    print(filled)
-    ax.voxels(data, facecolors=colors)
+    # Get max 
+    min = -np.array(robosys_grid.origin)
+    max = np.array(robosys_grid.map.shape) * robosys_grid.cell_size - robosys_grid.origin
 
-
-    # Adjust axes to work witht he grid
-    x_labels = (ax.get_xticks()*robosys_grid.cell_size-robosys_grid.origin.x)/100
-    y_labels = (ax.get_yticks()*robosys_grid.cell_size-robosys_grid.origin.y)/100
-    z_labels = (ax.get_zticks()*robosys_grid.cell_size-robosys_grid.origin.z)/100
-    ax.set_xticklabels(x_labels)
-    ax.set_yticklabels(y_labels)
-    ax.set_zticklabels(z_labels)
-    
-    plt.show()
-
-
-    # ## Uncomment below for plotly
-    # import plotly.graph_objects as go
-    # plots = data.nonzero()
-
-    # fig = go.Figure(data=[go.Scatter3d(x=plots[0], y=plots[1], z=plots[2], opacity=.05,
-    #                                 mode='markers')])
-    # fig.update_layout(
-    #     scene = dict(
-    #         xaxis = dict(nticks=4, range=[0,data.shape[0]],),
-    #                     yaxis = dict(nticks=4, range=[0,data.shape[1]],),
-    #                     zaxis = dict(nticks=4, range=[0,data.shape[2]],),),
-    #     margin=dict(r=20, l=10, b=10, t=10))
-    # # fig.update_layout(scene_aspectmode='cube')
-    # fig.show()
-    # # More work here: https://plotly.com/python/3d-axes/
+    fig = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=adj[0],
+                y=adj[1],
+                z=adj[2],
+                opacity=1,
+                mode="markers",
+                marker=dict(
+                    size=5,
+                    symbol="square",  # ['circle', 'circle-open', 'cross', 'diamond','diamond-open', 'square', 'square-open', 'x']
+                    # color=plots[0],                # set color to an array/list of desired values
+                    # colorscale='Viridis',   # choose a colorscale
+                    opacity=0.25,
+                ),
+                name="Obstacle Map"
+            ),
+            go.Scatter3d(
+                x=[0.],
+                y=[0.],
+                z=[0.],
+                opacity=1,
+                mode="markers",
+                marker=dict(
+                    size=5,
+                    symbol="circle",  # ['circle', 'circle-open', 'cross', 'diamond','diamond-open', 'square', 'square-open', 'x']
+                    color='black',                # set color to an array/list of desired values
+                    # colorscale='Viridis',   # choose a colorscale
+                ),
+                name="Origin"
+            ),
+                        go.Scatter3d(
+                x=[0.],
+                y=[0.],
+                z=[0.],
+                opacity=1,
+                mode="markers+text",
+                text=["Origin"],
+                textposition="top center",
+                marker=dict(
+                    size=5,
+                    symbol="circle",  # ['circle', 'circle-open', 'cross', 'diamond','diamond-open', 'square', 'square-open', 'x']
+                    color='black',                # set color to an array/list of desired values
+                    # colorscale='Viridis',   # choose a colorscale
+                ),
+                showlegend=False
+            ),
+        ]
+    )
+    fig.update_layout(
+        scene=dict(
+            camera = dict(eye=dict(x=0., y=-0.01, z=1)),
+            xaxis=dict(
+                nticks=robosys_grid.map.shape[0],
+                range=[min[0], max[0]],
+            ),
+            yaxis=dict(
+                nticks=robosys_grid.map.shape[1],
+                range=[min[1], max[1]],
+            ),
+            zaxis=dict(
+                nticks=robosys_grid.map.shape[2],
+                range=[min[2], max[2]],
+            ),
+        ),
+        margin=dict(r=5, l=5, b=5, t=5),
+    )
+    fig.show()
+    # More work here: https://plotly.com/python/3d-axes/
